6a30a3e65a7d9a7ce564269acb9362b6976745d1
marika <marika@waifu.club>
2019-11-07 09:23:25 -0400

    Initial commit.

---
+++ Little/Little.h
@@ -0,0 +1,15 @@
+//
+//  Little.h
+//  Little
+//
+//  Created by Marika on 2019-11-07.
+//  Copyright © 2019 Marika. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Little.
+FOUNDATION_EXPORT double LittleVersionNumber;
+
+//! Project version string for Little.
+FOUNDATION_EXPORT const unsigned char LittleVersionString[];
---
+++ LittleTests/LittleTests.swift
@@ -0,0 +1,14 @@
+//
+//  LittleTests.swift
+//  LittleTests
+//
+//  Created by Marika on 2019-11-07.
+//  Copyright © 2019 Marika. All rights reserved.
+//
+
+import XCTest
+@testable import Little
+
+class LittleTests: XCTestCase {
+
+}
