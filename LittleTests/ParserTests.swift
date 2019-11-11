2ec1e1bcb28276a8277fe7eb594fd476b31cbaa8
marika <marika@waifu.club>
2019-11-11 11:29:42 -0400

    Update ParserTests to handle Line ranges.

--- LittleTests/ParserTests.swift
+++ LittleTests/ParserTests.swift
@@ -24,15 +24,16 @@ class ParserTests: XCTestCase {
             """,
             equals: [
                 // A = 5
-                Line(
+                ExpectedLine(
                     number: nil,
                     statement: .assignment(
                         variable: "A",
                         value: Expression(root: .integer(value: 5))
-                    )
+                    ),
+                    literal: "A = 5"
                 ),
                 // 10 B = A / 2
-                Line(
+                ExpectedLine(
                     number: 10,
                     statement: .assignment(
                         variable: "B",
@@ -41,10 +42,11 @@ class ParserTests: XCTestCase {
                             left: .variable(name: "A"),
                             right: .integer(value: 2)
                         ))
-                    )
+                    ),
+                    literal: "10 B = A / 2"
                 ),
                 // 20 C = B * 25 - A
-                Line(
+                ExpectedLine(
                     number: 20,
                     statement: .assignment(
                         variable: "C",
@@ -57,10 +59,11 @@ class ParserTests: XCTestCase {
                             ),
                             right: .variable(name: "A")
                         ))
-                    )
+                    ),
+                    literal: "20 C = B * 25 - A"
                 ),
                 // Z = A / B / C
-                Line(
+                ExpectedLine(
                     number: nil,
                     statement: .assignment(
                         variable: "Z",
@@ -73,7 +76,8 @@ class ParserTests: XCTestCase {
                                 right: .variable(name: "C")
                             )
                         ))
-                    )
+                    ),
+                    literal: "Z = A / B / C"
                 ),
             ]
         )
@@ -86,21 +90,22 @@ class ParserTests: XCTestCase {
             A = 5
             PRINT A
             10 PRINT "A="; A
-            20 PRINT "A / 5 =", A / 5; "!"
+            20 PR "A / 5 =", A / 5; "!"
             30 PRINT A, "/", 5, "=", A / 5,
-            40 PRINT A; "/"; 5; "="; A / 5;
+            40 PR A; "/"; 5; "="; A / 5;
             """,
             equals: [
                 // A = 5
-                Line(
+                ExpectedLine(
                     number: nil,
                     statement: .assignment(
                         variable: "A",
                         value: Expression(root: .integer(value: 5))
-                    )
+                    ),
+                    literal: "A = 5"
                 ),
                 // PRINT A
-                Line(
+                ExpectedLine(
                     number: nil,
                     statement: .print(
                         items: [
@@ -109,10 +114,11 @@ class ParserTests: XCTestCase {
                                 terminator: ""
                             ),
                         ]
-                    )
+                    ),
+                    literal: "PRINT A"
                 ),
                 // 10 PRINT "A="; A
-                Line(
+                ExpectedLine(
                     number: 10,
                     statement: .print(
                         items: [
@@ -125,10 +131,11 @@ class ParserTests: XCTestCase {
                                 terminator: ""
                             ),
                         ]
-                    )
+                    ),
+                    literal: "10 PRINT \"A=\"; A"
                 ),
-                // 20 PRINT "A / 5 =", A / 5; "!"
-                Line(
+                // 20 PR "A / 5 =", A / 5; "!"
+                ExpectedLine(
                     number: 20,
                     statement: .print(
                         items: [
@@ -149,10 +156,11 @@ class ParserTests: XCTestCase {
                                 terminator: ""
                             ),
                         ]
-                    )
+                    ),
+                    literal: "20 PR \"A / 5 =\", A / 5; \"!\""
                 ),
                 // 30 PRINT A, "/", 5, "=", A / 5,
-                Line(
+                ExpectedLine(
                     number: 30,
                     statement: .print(
                         items: [
@@ -181,10 +189,11 @@ class ParserTests: XCTestCase {
                                 terminator: " "
                             ),
                         ]
-                    )
+                    ),
+                    literal: "30 PRINT A, \"/\", 5, \"=\", A / 5,"
                 ),
-                // 40 PRINT A; "/"; 5; "="; A / 5;
-                Line(
+                // 40 PR A; "/"; 5; "="; A / 5;
+                ExpectedLine(
                     number: 40,
                     statement: .print(
                         items: [
@@ -213,7 +222,8 @@ class ParserTests: XCTestCase {
                                 terminator: ""
                             ),
                         ]
-                    )
+                    ),
+                    literal: "40 PR A; \"/\"; 5; \"=\"; A / 5;"
                 ),
             ]
         )
@@ -224,11 +234,41 @@ class ParserTests: XCTestCase {
     /// Assert that the lines parsed from the given input match the given lines.
     /// - Parameter input: The input to parse the lines of.
     /// - Parameter expected: The lines that are expected to be parsed.
-    private func assertLines(from input: String, equals expected: [Line]) {
+    private func assertLines(from input: String, equals expected: [ExpectedLine]) {
         let lexer = Lexer(input: input)
         let parser = Parser(lexer: lexer)
-        var got = [Line]()
-        XCTAssertNoThrow(got = try parser.parse())
+        var lines = [Line]()
+        XCTAssertNoThrow(lines = try parser.parse())
+
+        let got = lines.map {
+            ExpectedLine(
+                number: $0.number,
+                statement: $0.statement,
+                literal: String(input[$0.range])
+            )
+        }
+
         XCTAssertEqual(got, expected)
+    }
+}
+
+// MARK: ExpectedLine
+
+extension ParserTests {
+    /// The data structure for defining an expected line result when asserting parsed lines.
+    ///
+    /// This is simply because manually entering the string ranges would be terrible and prone to error.
+    private struct ExpectedLine: Equatable {
+
+        // MARK: Public Properties
+
+        /// The expected number of the line.
+        let number: Int?
+
+        /// The expected statement of the line.
+        let statement: Statement
+
+        /// The expected string in the range of the line.
+        let literal: String
     }
 }
